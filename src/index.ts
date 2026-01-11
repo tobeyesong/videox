3f5705488d8987783f8cbc743e77d16e5a56079a
Toan Lam <toanlam01@gmail.com>
2026-01-10 23:55:05 -0800

    Add Google Cloud Storage integration for video processing

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,61 @@
 import expres from 'express';
-import ffmpeg from 'fluent-ffmpeg';
+import { 
+  convertVideo, 
+  downloadRawVideo, 
+  setupDirectories, 
+  uploadProcessedVideo,
+  deleteRawVideo,
+  deleteProcessedVideo
+} from './storage';
+import { upload } from '@google-cloud/storage/build/cjs/src/resumable-upload';
+
+setupDirectories();
 
 const app  = expres();
 
 app.use(expres.json());
 
-app.post('/process-video', (req, res) => {
+app.post('/process-video', async (req, res) => {
+  //Get the bucket and filename from Cloud Pub/Sub message
+  let data;
+  try {
+    const message = Buffer.from(req.body.message.data, 'base64').toString('utf8');
+    data = JSON.parse(message);
+    if (!data.name){
+      throw new Error('Invalid message payload recieved.');
+    }
+  } catch (error) {
+    console.error('Error parsing Pub/Sub message:', error);
+    res.status(400).send('Invalid Pub/Sub message payload.');
+    return;  
+  }
 
+  const inputFileName = data.name;
+  const outputFileName = `processed-${inputFileName}`;
 
-  // Get path of the inptut video file from request body
-  const inputFilePath = req.body.inputFilePath;
-  const outputFilePath = req.body.outputFilePath;
+  // Donwload the raw video from Cloud Storage
+  await downloadRawVideo(inputFileName);
 
-  if (!inputFilePath || !outputFilePath) {
-  const missing = [];
-  if (!inputFilePath) missing.push('input');
-  if (!outputFilePath) missing.push('output');
+  // Convert the video to 360p resolution
+  try {
+    await convertVideo(inputFileName, outputFileName);
+  } catch (error) {
+    Promise.all([
+      deleteRawVideo(inputFileName),
+      deleteProcessedVideo(outputFileName)
+    ])
+    return res.status(500).send('Error processing video.');
+  }
 
-  return res
-    .status(400)
-    .send(`Missing ${missing.join(' and ')} file path(s).`);
-}
+  // Upload the processed video to Cloud Storage
+  await uploadProcessedVideo(outputFileName);
 
-  ffmpeg(inputFilePath)
-  .outputOptions('-vf', 'scale=-1:360')
-  .on('end', () => {
-    res
-      .status(200)
-      .send(`Video processed successfully. Saved to ${outputFilePath}`);
-  })
-  .on('error', (err) => {
-    console.error(`Error processing video: ${err.message}`);
-    res
-      .status(500)
-      .send(`Error processing video: ${err.message}`);
-  })
-  .save(outputFilePath);
+  await Promise.all([
+      deleteRawVideo(inputFileName),
+      deleteProcessedVideo(outputFileName)
+    ])
+
+    return res.status(200).send('Video processed successfully.');
 
 
 });
